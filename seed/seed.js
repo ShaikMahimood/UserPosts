b0efc28dbd96d630ad3902d2119d483be24f3909
mahimoodShaik <mahshaik@innominds.com>
2023-01-20 16:55:45 +0530

    Updated Seed data code

--- seed/seed.js
+++ seed/seed.js
@@ -1,36 +1,12 @@
-const { getUsers } = require("../controller/user");
+const { getUsers, insertUsers } = require("../controller/user");
 const { insertPosts } = require("../controller/posts");
 
-const post = [
-  {
-    title: "tresure island",
-    description:
-      "Treasure Island is the story of a boy who sails on a ship searching for treasure",
-  },
+const users = require("../files/users.json");
+const posts = require("../files/posts.json");
 
-  {
-    title: "The adventure of tom sawyer",
-    description:
-      "Tom Sawyer is a troublemaking little boy who is always causing problems, having fun and enjoying many crazy adventures",
-  },
-
-  {
-    title: "The Story of Doctor Dolittle",
-    description:
-      "Doctor Dolittle loves animals. He loves them so much that when his many pets scare away his human patients, he learns how to talk to animals and becomes a veterinarian instead",
-  },
-  {
-    title: " The Secret Garden",
-    description:
-      "The Secret Garden is a touching story about the power of friendship",
-  },
-
-  {
-    title: "The Picture of Dorian Gray",
-    description:
-      "Dorian Gray makes a deal to stay young forever—while a painted portrait of him shows all the signs of aging",
-  },
-];
+async function seedUsersData() {
+  await insertUsers(users);
+}
 
 async function getUserId() {
   const users = await getUsers();
@@ -41,15 +17,20 @@ async function getUserId() {
   return userId;
 }
 
-async function seedData() {
+async function seedPostsData() {
   const users = await getUserId();
-  for (i = 0; i < users.length; i++) {
-
+  for (i = 0; i < 3; i++) {
+    const post = posts;
     for (j = 0; j < post.length; j++) {
       post[j]["userId"] = users[i];
     }
-    await insertPosts(post[i]);
+    await insertPosts(post);
   }
 }
 
-seedData();
+async function main() {
+  await seedUsersData();
+  seedPostsData();
+}
+
+main();
